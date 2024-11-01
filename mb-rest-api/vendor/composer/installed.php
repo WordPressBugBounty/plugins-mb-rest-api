0eab276d63b637c42eda5a8afa60e9c0392c3b3b
Sean Murphy <sean@iamseanmurphy.com>
2024-11-01 03:39:18 +0000

    2.0.5

--- mb-rest-api/vendor/composer/installed.php
+++ mb-rest-api/vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
         'name' => 'wpmetabox/mb-rest-api',
-        'pretty_version' => '2.0.4',
-        'version' => '2.0.4.0',
-        'reference' => '0982f87af583a5e088b7bbf90234b9e1c390ee36',
+        'pretty_version' => '2.0.5',
+        'version' => '2.0.5.0',
+        'reference' => '67a67db7cd078f1994bcbb373b2777a38f1333aa',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'dev' => true,
+        'dev' => false,
     ),
     'versions' => array(
         'wpmetabox/mb-rest-api' => array(
-            'pretty_version' => '2.0.4',
-            'version' => '2.0.4.0',
-            'reference' => '0982f87af583a5e088b7bbf90234b9e1c390ee36',
+            'pretty_version' => '2.0.5',
+            'version' => '2.0.5.0',
+            'reference' => '67a67db7cd078f1994bcbb373b2777a38f1333aa',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
